1e343950e6f23e468254cba8d4c31f4fa2b54eb7
siddhantc1999 <schiring369@gmail.com>
2023-05-09 10:19:12 +0530

    setting the angle to line renderer

--- Assets/linerenderer.cs
+++ Assets/linerenderer.cs
@@ -12,9 +12,15 @@ public class linerenderer : MonoBehaviour
     public List<LineRenderer> linerenderers;
     public EdgeCollider2D myedgecollier2d;
     public List<Vector2> positions;
-   
-    // Start is called before the first frame update
-    [System.Obsolete]
+    public Vector3 fromvector;
+    public Vector3 tovector;
+    spriteboundaries myspriteboundaries;
+    public float angle;
+    float timer = 0f;
+    bool linemove = false;
+    float perc;
+    // Start is called before the first frame update
+    [System.Obsolete]
     void Start()
     {
         mylinerenderer = GetComponent<LineRenderer>();
@@ -22,34 +28,45 @@ public class linerenderer : MonoBehaviour
         mylinerenderer.SetWidth(0.1f, 0.1f);
         previouspos = transform.position;
         currentposition = new Vector3(0, 0, 0);
-
+        myspriteboundaries = FindObjectOfType<spriteboundaries>();
     }
 
-    // Update is called once per frame
-    void Update()
+    // Update is called once per frame
+    void Update()
     {
 
 
-        if(Input.GetMouseButton(0))
+        if (Input.GetMouseButton(0))
         {
             ////linecount 0 differtent and more than that its different
 
-             currentposition +=  (transform.up * 0.9f*Time.deltaTime);
+            currentposition += (transform.up * 0.9f * Time.deltaTime);
             //sprite.transform.position = currentposition;
-             positions.Add(new Vector2(currentposition.x, currentposition.y));
-             mylinerenderer.positionCount++;
-             mylinerenderer.SetPosition(mylinerenderer.positionCount - 1, currentposition);
+            positions.Add(new Vector2(currentposition.x, currentposition.y));
+            mylinerenderer.positionCount++;
+            mylinerenderer.SetPosition(mylinerenderer.positionCount - 1, currentposition);
 
             myedgecollier2d.points = positions.ToArray();
-          
+
+
 
 
         }
-        //if(Input.GetMouseButtonUp(0))
-        //{
-        //    currentposition= new Vector3(0, 0, 0);
-          
-        //}
+        if (Input.GetMouseButtonUp(0))
+        {
+            linemove = true;
+            fromvector = new Vector3(currentposition.x, currentposition.y, 0f) - Vector3.zero;
+            tovector = myspriteboundaries.differencevector;
+            angle = Vector3.Angle(fromvector, tovector);
+
+
+        }
+        if (timer <= 1f && linemove)
+        {
+            perc = Mathf.Lerp(0f, angle, timer);
+            timer += Time.deltaTime;
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, -perc);
+        }
 
     }
 }
--- Assets/spriteboundaries.cs
+++ Assets/spriteboundaries.cs
@@ -5,20 +5,36 @@ using UnityEngine;
 public class spriteboundaries : MonoBehaviour
 {
     SpriteRenderer myspriterenderer;
-    public Vector3 threshold;
-    // Start is called before the first frame update
-    void Start()
+    /*   public Vector3 threshold;*/
+    /* public Vector3 size;*/
+    PolygonCollider2D mypolygoncollider2d;
+    /* public Vector3 centre;*/
+    public Vector3 xmin;
+    public Vector3 xmax;
+    public Vector3 toposition;
+    public Vector3 differencevector;
+    public float timer = 0;
+    public float perc = 0f;
+    // Start is called before the first frame update
+    void Start()
     {
         myspriterenderer = GetComponent<SpriteRenderer>();
-        threshold = 2*myspriterenderer.bounds.extents;
-      
-        //Debug.Log("the boundaries of a spriute renderer" + myspriterenderer.bounds.extents);
+        mypolygoncollider2d = GetComponent<PolygonCollider2D>();
+
+
+        xmin = mypolygoncollider2d.bounds.min;
+        xmax = mypolygoncollider2d.bounds.max;
+        toposition = new Vector3(xmin.x, xmax.y, 0f);
+        differencevector = toposition - Vector3.zero;
+
     }
 
-    // Update is called once per frame
-    void Update()
+    // Update is called once per frame
+    void Update()
     {
-   
-        //Debug.Log("the width"+s);
+        /* timer += Time.deltaTime;*/
+        Debug.DrawLine(Vector3.zero, toposition, Color.red);
+
+
     }
 }
